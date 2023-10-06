2d8536b25a68e56ec4a7b3c0c7e4845ad83f9bc9
BEKIRKSU <bekirkscu@gmail.com>
2023-10-06 14:02:46 +0100

    Snake made

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
 
+snake_1 = Turtle("square")
+snake_1.color("white")
 
+snake_2 = Turtle("square")
+snake_2.color("white")
+snake_2.goto(-20, 0)
 
-
+snake_3 = Turtle("square")
+snake_3.color("white")
+snake_3.goto(-40, 0)
 
 
 
