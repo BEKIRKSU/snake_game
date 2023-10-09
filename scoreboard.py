81babea5e080d5267f13952437623b2de8364b45
BEKIRKSU <bekirkscu@gmail.com>
2023-10-09 16:23:34 +0100

    Score written along the top

--- scoreboard.py
+++ scoreboard.py
@@ -6,4 +6,9 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        self.penup()
+        self.goto(x=-50, y=270)
+        # these ^ need to be addressed before the writing happens.
         self.write(f"Score: {self.score}", align="left", font=('Arial', 24, 'normal'))
+        self.hideturtle()
+
