4f4bfff8e8a3015dc84b6bbcc4b31b17ddbc03f2
BEKIRKSU <bekirkscu@gmail.com>
2023-10-09 16:29:43 +0100

    Code cleaned up by making functions and constants

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-
+ALIGNMENT = "center"
+FONT = ('Arial', 24, 'normal')
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -13,7 +14,7 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="left", font=('Arial', 24, 'normal'))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
